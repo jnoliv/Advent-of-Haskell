7dcdee01e85bcd9df298db96df0a9e2cf7b3e5dc
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-01 11:49:15 +0000

    #Day01: solve the second part and add some documentation
    Fix a number and then use the two pointer technique to see if any pair
    matches the new sum.
    
    Also added some documentation to the functions.

--- day01/day01.hs
+++ day01/day01.hs
@@ -1,5 +1,10 @@
 import Data.List (sort)
 
+-- | Finds a pair in the given list that sums to the given number.
+-- Uses the two pointer technique to do so, which is probably not the
+-- most efficient way to do this in Haskell. This assumes a sorted input
+-- list! Check the algorithm here:
+-- https://www.geeksforgeeks.org/two-pointers-technique/
 findPair :: Integer -> Int -> Int -> [Integer] -> Maybe (Integer, Integer)
 findPair _ _ _ [] = Nothing
 findPair sum l r list
@@ -10,7 +15,21 @@ findPair sum l r list
     where
         left  = list !! l
         right = list !! r
-        curSum = left + right 
+        curSum = left + right
+
+-- | Finds a triplet in the given list that sums to the given number.
+-- Fixes a number and then uses the two pointer technique to see if any pair
+-- matches the new sum (the given sum minus the fixed number). Tries it for
+-- every number until a match or the end of the list. Assumes a sorted input
+-- list!
+findTriplet :: Integer -> [Integer] -> Maybe (Integer, Integer, Integer)
+findTriplet _ [] = Nothing
+findTriplet sum (x:xs) =
+    let newSum = sum - x
+        lastInd = length xs - 1 
+    in case findPair newSum 0 lastInd xs of
+        Just (y,z) -> Just (x,y,z)
+        Nothing    -> findTriplet sum xs
 
 main :: IO()
 main = do
@@ -18,8 +37,10 @@ main = do
     let expenses = sort . map read . lines $ contents
     let size = length expenses
 
-    print size
-
     case findPair 2020 0 (size - 1) expenses of
-        Just (x,y) -> putStrLn $ "Result: " ++ show (x * y)
-        Nothing   -> putStrLn "No entries sum to 2020"
+        Just (x,y) -> putStrLn $ "Pair multiplication: " ++ show (x * y)
+        Nothing    -> putStrLn "No two entries sum to 2020"
+
+    case findTriplet 2020 expenses of
+        Just (x,y,z) -> putStrLn $ "Triplet multiplication: " ++ show (x * y * z)
+        Nothing      -> putStrLn "No three entries sum to 2020"

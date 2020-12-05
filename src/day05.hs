f44dd08d2aff45fa793d22efaba533fe30b4407d
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-05 09:11:04 +0000

    #Day05: solve part 2, just sort and find the gap

--- src/day05.hs
+++ src/day05.hs
@@ -1,5 +1,7 @@
 import qualified Common.AdventAPI as AdventAPI
 
+import Data.List (sort)
+
 bspToBit :: Char -> Int
 bspToBit c
     | c == 'F' || c == 'L' = 0
@@ -8,8 +10,15 @@ bspToBit c
 binToDec :: [Int] -> Int
 binToDec = foldl (\acc bit -> 2*acc + bit) 0
 
+findSeat :: [Int] -> Int
+findSeat (n:m:ss) = if n == pred m then findSeat (m:ss) else succ n
+findSeat _ = -1
+
 main :: IO()
 main = do
     contents <- AdventAPI.readInput 5 "../session-cookie.txt" "../input"
 
-    print . maximum . map (binToDec . map bspToBit) . lines $ contents
+    let seats = sort . map (binToDec . map bspToBit) . lines $ contents
+
+    print . maximum $ seats
+    print . findSeat $ seats

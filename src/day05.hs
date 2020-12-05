5d492670bf1f015f43d356656cd6cec0e2eb4f3f
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-05 16:03:03 +0000

    #Day05: add input file and some comments

--- src/day05.hs
+++ src/day05.hs
@@ -2,17 +2,20 @@ import qualified Common.AdventAPI as AdventAPI
 
 import Data.List (sort)
 
+-- | Parse a character of the binary space partioning into a bit
 bspToBit :: Char -> Int
 bspToBit c
     | c == 'F' || c == 'L' = 0
     | c == 'B' || c == 'R' = 1
 
+-- | Convert a list of bits to a decimal number
 binToDec :: [Int] -> Int
 binToDec = foldl (\acc bit -> 2*acc + bit) 0
 
+-- | Find the gap in a sorted list of seats
 findSeat :: [Int] -> Int
 findSeat (n:m:ss) = if n == pred m then findSeat (m:ss) else succ n
-findSeat _ = -1
+findSeat _ = error "No free seat exists"
 
 main :: IO()
 main = do

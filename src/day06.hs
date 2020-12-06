e7ca037c2849f9b5c87dfe0f9fdbd9ec6203406c
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-06 08:56:01 +0000

    #Day06: solve second part by intersecting the strings

--- src/day06.hs
+++ src/day06.hs
@@ -1,18 +1,22 @@
 import qualified Common.AdventAPI as AdventAPI
 
 import qualified Data.Set as Set
+import Data.List (intersect)
 import Data.List.Split (splitOn)
 
 type AnswerGroup = Set.Set Char
 
-parseAnswers :: [String] -> [AnswerGroup]
-parseAnswers = map (Set.fromList . filter (/='\n'))
+parseAnswers1 :: [String] -> [AnswerGroup]
+parseAnswers1 = map (Set.fromList . filter (/='\n'))
+
+parseAnswers2 :: [String] -> [String]
+parseAnswers2 = map (foldr1 intersect . lines)
 
 main :: IO()
 main = do
     contents <- AdventAPI.readInput 6 "../session-cookie.txt" "../input"
 
-    let groupAnswers = parseAnswers $ splitOn "\n\n" contents
-        sumAnswers = sum . map Set.size $ groupAnswers
+    let groups = splitOn "\n\n" contents
 
-    print sumAnswers
+    print . sum . map Set.size . parseAnswers1 $ groups
+    print . sum . map length . parseAnswers2 $ groups

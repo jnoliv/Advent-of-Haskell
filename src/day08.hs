53e869a51959a281f987483bbb8885f19c301d31
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-08 14:36:01 +0000

    #Day08: cosmetic changes

--- src/day08.hs
+++ src/day08.hs
@@ -1,29 +1,32 @@
 import qualified Common.AdventAPI as AdventAPI
 
-import Data.Maybe (fromJust)
-import qualified Data.Set    as Set
+import Data.Maybe (maybe)
+import qualified Data.IntSet    as Set
 import qualified Data.Vector as Vector
 
 type Op      = String
 type Program = Vector.Vector (Op, Int)
 
+-- | Parse an input line into a (operation, argument) pair
 parseOp :: String -> (Op, Int)
 parseOp line = (op, arg)
     where [op,rArg] = words line
           arg       = if head rArg == '+' then read $ tail rArg else read rArg
 
-executeUntilLoop :: Program -> Set.Set Int -> Int -> Int -> (Bool, Int)
+-- | Execute the program until either it succeeds or a loop is detected
+executeUntilLoop :: Program -> Set.IntSet -> Int -> Int -> (Bool, Int)
 executeUntilLoop program visited pc acc
     | pc `Set.member` visited     = (False, acc)
-    | pc >= Vector.length program = (True, acc)
+    | pc >= Vector.length program = (True,  acc)
     | otherwise                   =
         case op of
-            "nop" -> executeUntilLoop program newVisited (succ pc) acc
-            "acc" -> executeUntilLoop program newVisited (succ pc) (acc + arg)
-            "jmp" -> executeUntilLoop program newVisited (pc + arg) acc
+            "nop" -> executeUntilLoop program visited' (succ pc)  acc
+            "acc" -> executeUntilLoop program visited' (succ pc) (acc + arg)
+            "jmp" -> executeUntilLoop program visited' (pc + arg) acc
         where (op,arg)   = program Vector.! pc
-              newVisited = Set.insert pc visited
+              visited' = Set.insert pc visited
 
+-- | Brute force changing nops to jmps (and vice-versa) until the program terminates
 tryRestoreRun :: Program -> Int -> Maybe Int
 tryRestoreRun program adr
     | adr >= Vector.length program        = Nothing
@@ -32,8 +35,8 @@ tryRestoreRun program adr
         if success then Just result else tryRestoreRun program (succ adr)
         where uncorrupt ("jmp",arg) = ("nop",arg)
               uncorrupt ("nop",arg) = ("jmp",arg)
-              newProgram            = program Vector.// [(adr, uncorrupt $ program Vector.! adr)]
-              (success, result)     = executeUntilLoop newProgram Set.empty 0 0
+              program'              = program Vector.// [(adr, uncorrupt $ program Vector.! adr)]
+              (success, result)     = executeUntilLoop program' Set.empty 0 0
 
 main :: IO()
 main = do
@@ -44,4 +47,4 @@ main = do
         result2 = tryRestoreRun program 0
 
     print . snd $ result1
-    print . fromJust $ result2
+    putStrLn . maybe "could not restore program" show $ result2

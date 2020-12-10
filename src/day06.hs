c1506c4cc5cffda45890129eaedaf79f3e0808c9
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-10 15:41:19 +0000

    #Day06: minor aesthetic changes, basically

--- src/day06.hs
+++ src/day06.hs
@@ -7,8 +7,8 @@ type AG = Set.Set Char  -- answer group
 
 -- | Fold each group using 'f' with 'zero' as the base case and
 -- return the sum of the sizes of each folded group
-solve :: [[String]] -> (AG -> AG -> AG, AG) -> Int
-solve answers (f, zero) = sum . map (Set.size . foldr foldFunc zero) $ answers
+solve :: [[String]] -> (AG -> AG -> AG) -> AG -> Int
+solve answers f zero = sum . map (Set.size . foldr foldFunc zero) $ answers
     where foldFunc x set = f set $ Set.fromList x
 
 main :: IO()
@@ -16,4 +16,4 @@ main = do
     contents <- AdventAPI.readInputDefaults 6
 
     let groups = map words . splitOn "\n\n" $ contents
-    mapM_ (print . solve groups) [(Set.union, Set.empty), (Set.intersection, Set.fromList ['a'..'z'])]
+    mapM_ print $ zipWith (solve groups) [Set.union, Set.intersection] [Set.empty, Set.fromList ['a'..'z']]

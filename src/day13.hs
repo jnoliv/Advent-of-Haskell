8a3d39a86834f5698f38881b98451fc50a34abf7
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-13 21:57:31 +0000

    #Day13: chinese remainder theorem, huh?
    I'll eventually implement it instead of using arithmoi. Just let me
    figure this extended euclidian algorithm first.

--- src/day13.hs
+++ src/day13.hs
@@ -1,28 +1,33 @@
 import AdventAPI (readInputDefaults)
 
-import Data.Function (on)
-import Data.List (minimumBy)
+import Data.Maybe (fromJust)
 import Data.List.Split (splitOn)
+import Math.NumberTheory.Moduli.Chinese (chineseRemainder)
 
 -- | Find the smallest multiple of 'n' that is bigger than 'limit'
-smallestMultBiggerThan :: Int -> Int-> Int
-smallestMultBiggerThan limit n = limit + n - (limit `mod` n)
+waitTime :: Integral a => a -> a-> a
+waitTime limit n = n - (limit `mod` n)
 
 -- | Find the earliest departure bus and time after 't'
-findEarliestDeparture :: Int -> [Int] -> (Int, Int)
-findEarliestDeparture t = minimumBy (compare `on` snd) . map ((,) <$> id <*> smallestMultBiggerThan t)
+findEarliestDeparture :: Integral a => a -> [a] -> (a, a)
+findEarliestDeparture t = minimum . map ((,) <$> waitTime t <*> id)
 
-parseInput :: String -> (Int, [Int])
+-- | Prepare to input to 'chineseRemainder'
+toChinRemInput :: [(Integer,Integer)] -> [(Integer,Integer)]
+toChinRemInput = map (\(n,a) -> (waitTime a n, n))
+
+parseInput :: String -> (Integer, [(Integer,Integer)])
 parseInput input = (t, ids)
     where ls = lines input
           t   = read . head $ ls
-          ids = map read . filter (/= "x") . splitOn "," . (!! 1) $ ls  
+          ids = map ((,) <$> read . fst <*> snd) . filter ((/=) "x" . fst) . (`zip` [0..]) . splitOn "," . (!! 1) $ ls
 
 main :: IO()
 main = do
     contents <- AdventAPI.readInputDefaults 13
 
     let (t, ids) = parseInput contents
-        depart   = findEarliestDeparture t ids
+        depart   = findEarliestDeparture t $ map fst ids
 
-    print $ fst depart * (snd depart - t)
+    print . uncurry (*) $ depart
+    print . fromJust . chineseRemainder . toChinRemInput $ ids

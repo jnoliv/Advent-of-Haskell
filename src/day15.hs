f98e1c166cfc7b8a239e60dd8aeec38f5960862d
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-15 17:48:35 +0000

    #Day15: use records instead of triples, it's neater

--- src/day15.hs
+++ src/day15.hs
@@ -2,7 +2,8 @@ import AdventAPI (readInputDefaults)
 import qualified Data.IntMap as M
 import Data.List.Split (splitOn)
 
-type Mem = M.IntMap (Int,Int)   -- (last said, previous last said)
+type Mem  = M.IntMap (Int,Int)   -- (last said, previous last said)
+data Game = Game { turn :: Int, prev :: Int, mem :: Mem}
 
 age :: Int -> Mem -> Int
 age n = uncurry (-) . M.findWithDefault (0,0) n
@@ -11,16 +12,10 @@ say :: Int -> Int -> Mem -> Mem
 say n turn mem = M.insert n turns mem
     where turns = (\(t1,_) -> (turn, t1)) $ M.findWithDefault (turn, turn) n mem
 
-play :: (Int, Int, Mem) -> (Int, Int, Mem)
-play (turn, last, mem) = (succ turn, a, mem')
+play :: Game -> Game
+play (Game turn last mem) = Game (succ turn) a mem'
     where a    = age last mem
           mem' = say a turn mem
-
-fst3 :: (a,b,c) -> a
-fst3 (a,_,_) = a
-
-snd3 :: (a,b,c) -> b
-snd3 (_,b,_) = b
 
 main :: IO()
 main = do
@@ -28,8 +23,8 @@ main = do
 
     let startNums = map read . splitOn "," $ contents
         mem       = M.fromList . zip startNums $ zip [0..] [0..]
-        n2020     = until ((==) 2020 . fst3) play (length startNums, last startNums, mem)
-        n30000000 = until ((==) 30000000 . fst3) play (length startNums, last startNums, mem)
+        n2020     = until ((==)     2020 . turn) play $ Game (length startNums) (last startNums) mem
+        n30000000 = until ((==) 30000000 . turn) play $ Game (length startNums) (last startNums) mem
 
-    print . snd3 $ n2020
-    print . snd3 $ n30000000
+    print . prev $ n2020
+    print . prev $ n30000000

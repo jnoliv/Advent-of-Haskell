46179324b27cb9d57bc6a6089806dcd9a7b79941
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-21 16:37:41 +0000

    #Day21: I guess abominations are to be expected this late in the month

--- src/day21.hs
+++ src/day21.hs
@@ -1,6 +1,9 @@
 {-# Language OverloadedStrings, TupleSections #-}
 
 import Control.Applicative (many)
+import Data.Bifunctor (second)
+import Data.Function (on)
+import Data.List (sortBy, delete, intercalate)
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Utils (Parser, readParsedLines, count)
@@ -20,6 +23,13 @@ mergeAlgsPossibilities = foldl foldFunc M.empty . concatMap toAlgSet
     where foldFunc acc (k,v) = M.insertWith S.intersection k v acc
           toAlgSet (i,a)     = map (, S.fromList i) a
 
+mapAlgsToIngs :: Algs -> [(String, String)]
+mapAlgsToIngs = sortBy (compare `on` fst) . foldAlgs . sortByNIngs .  map (second S.toList) . M.toList
+    where sortByNIngs = sortBy (compare `on` (length . snd))
+          
+          foldAlgs [] = []
+          foldAlgs ((a,[i]) : algs) = (a,i) : (foldAlgs . sortByNIngs . map (second (delete i)) $ algs)
+
 main :: IO()
 main = do
     input <- readParsedLines 21 format
@@ -31,4 +41,7 @@ main = do
         potencialAlgs     = M.foldr S.union S.empty algsPossibilities
         safeIngredients   = uniqueIngredients S.\\ potencialAlgs
 
+        algsIngsMapping   = mapAlgsToIngs algsPossibilities
+
     print . count (`S.member` safeIngredients) $ allIngredients
+    putStrLn . intercalate "," . map snd $ algsIngsMapping

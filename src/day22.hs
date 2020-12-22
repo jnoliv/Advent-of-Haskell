7f58398b46b8d679f72487f9209a85b867f5a253
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-22 12:05:12 +0000

    #Day22: another abomination joins the club

--- src/day22.hs
+++ src/day22.hs
@@ -5,12 +5,17 @@ import Text.Megaparsec (endBy)
 import Text.Megaparsec.Char.Lexer (decimal)
 import Utils (Parser, parseWrapper)
 
+data GameSt = Ongoing | Win Int
+    deriving Eq
+
 type Game = ([Int], [Int])
+type RecGame = (Game, GameSt, [Game])
 
 format :: Parser Game
 format = (,) <$> ("Player 1:\n"   *> decimal `endBy` "\n")
              <*> ("\nPlayer 2:\n" *> decimal `endBy` "\n")
 
+-----------------------------------------------------------------
 turn :: Game -> Game
 turn (c1:cs1, c2:cs2) = 
     if c1 > c2
@@ -19,6 +24,25 @@ turn (c1:cs1, c2:cs2) =
 
 play :: Game -> Game
 play = until ((||) <$> null . fst <*> null . snd) turn
+
+-----------------------------------------------------------------
+
+playRec :: RecGame -> RecGame
+playRec = until (\(_, st, _) -> st /= Ongoing) turnRec
+
+turnRec :: RecGame -> RecGame
+turnRec (g@(_,[]), _, prevGames) = (g, Win 1, prevGames)
+turnRec (g@([],_), _, prevGames) = (g, Win 2, prevGames)
+turnRec (g@(c1:cs1, c2:cs2), _, prevGames)
+    | g `elem` prevGames = (g, Win 1, prevGames)
+    | length cs1 >= c1 && length cs2 >= c2 =
+        rearrange $ playRec ((take c1 cs1, take c2 cs2), Ongoing, [])
+    | otherwise =
+        rearrange (g, if c1 > c2 then Win 1 else Win 2, prevGames)
+    where rearrange (_, Win 1, _) = ((cs1 ++ [c1,c2], cs2), Ongoing, g : prevGames)
+          rearrange (_, Win 2, _) = ((cs1, cs2 ++ [c2,c1]), Ongoing, g : prevGames)
+
+-----------------------------------------------------------------
 
 score :: Game -> Int
 score = sum . zipWith (*) [1..] . reverse . winnerDeck
@@ -29,6 +53,5 @@ main :: IO()
 main = do
     gameStart <- parseWrapper format <$> readInputDefaults 22
 
-    let end = play gameStart
-
-    print $ score end
+    print . score $ play gameStart
+    print . score . (\(g,_,_) -> g) $ playRec (gameStart, Ongoing, [])

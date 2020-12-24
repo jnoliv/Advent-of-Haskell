a629152d37bfa403b03afec1165a2daa373f1e70
João Nuno Oliveira <joaonunoao@gmail.com>
2020-12-24 11:25:22 +0000

    #Day24: another celular automaton, heh?

--- src/day24.hs
+++ src/day24.hs
@@ -4,9 +4,10 @@ import Control.Applicative ((<|>))
 import Data.Functor (($>))
 import qualified Data.Set as Set
 import Text.Megaparsec (manyTill, lookAhead)
-import Utils (Parser, readParsedLines)
+import Utils (Parser, readParsedLines, count)
 
 type AxialCoord  = (Int,Int)
+type Tiles       = Set.Set AxialCoord
 data Instruction = E | SE | SW | W | NW | NE
 
 
@@ -21,24 +22,44 @@ instruction = ("e"  $> E)
           <|> ("nw" $> NW)
           <|> ("ne" $> NE)      
 
-move :: Instruction -> AxialCoord -> AxialCoord
-move E  (x,y) = (x - 1, y    )
-move SE (x,y) = (x - 1, y + 1)
-move SW (x,y) = (x    , y + 1)
-move W  (x,y) = (x + 1, y    )
-move NW (x,y) = (x + 1, y - 1)
-move NE (x,y) = (x    , y - 1)
+move :: AxialCoord -> Instruction -> AxialCoord
+move (x,y) E  = (x - 1, y    )
+move (x,y) SE = (x - 1, y + 1)
+move (x,y) SW = (x    , y + 1)
+move (x,y) W  = (x + 1, y    )
+move (x,y) NW = (x + 1, y - 1)
+move (x,y) NE = (x    , y - 1)
 
-identify :: Set.Set AxialCoord -> [Instruction] -> Set.Set AxialCoord
+identify :: Tiles -> [Instruction] -> Tiles
 identify tiles instructions
     | coords `Set.notMember` tiles = Set.insert coords tiles
     | otherwise                    = Set.delete coords tiles
-    where coords = foldl (flip move) (0,0) instructions
+    where coords = foldl move (0,0) instructions
+
+neighbours :: AxialCoord -> [AxialCoord]
+neighbours coord = map (move coord) [E, SE, SW, W, NW, NE]
+
+countBlackNeighbours :: Tiles -> AxialCoord -> Int
+countBlackNeighbours tiles = count (`Set.member` tiles) . neighbours
+
+keepBlack :: Int -> Bool
+keepBlack n = n == 1 || n == 2
+
+whiteNeighbours :: Tiles -> Tiles
+whiteNeighbours set = Set.filter (`Set.notMember` set) allNeighs
+    where allNeighs = Set.fromList . concatMap neighbours . Set.toList $ set
+
+flipAll :: Tiles -> Tiles
+flipAll set = keep `Set.union` new
+    where keep = Set.filter (keepBlack . countBlackNeighbours set) set
+          new  = Set.filter ((==2) . countBlackNeighbours set) (whiteNeighbours set)
 
 main :: IO()
 main = do
     instructions <- readParsedLines 24 format
 
-    let tiles = foldl identify Set.empty instructions
+    let tiles    = foldl identify Set.empty instructions
+        tiles100 = iterate flipAll tiles !! 100
 
     print . Set.size $ tiles
+    print . Set.size $ tiles100

ba9e1b396fa0d25f6eb60dd8924b114fad626ef2
João Nuno Oliveira <joaonunoao@gmail.com>
2021-01-04 18:42:08 +0000

    2015 day 01: took me a while to figure out is was using the wrong fold

--- src/2015/day01.hs
+++ src/2015/day01.hs
@@ -1,9 +1,19 @@
 import AdventAPI (readInputDefaults)
 
+toNum :: String -> [Int]
+toNum = map (\c -> if c == '(' then 1 else -1)
+
+stepsTilBasement :: [Int] -> Int
+stepsTilBasement = snd . foldl f (0,0)
+    where f r@(floor, steps) dir = if floor == -1 then r else (floor + dir, steps + 1)
+
 -- |
 -- >>> :main
 -- 232
+-- 1783
 main :: IO ()
 main = do
-    directions <- map (\c -> if c == '(' then 1 else -1) <$> readInputDefaults 2015 1
-    print . sum $ directions
+    steps <- toNum <$> readInputDefaults 2015 1
+
+    print . sum              $ steps
+    print . stepsTilBasement $ steps

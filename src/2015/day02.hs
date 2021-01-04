59046ef59899befde730b962ddad6b9200120bb0
João Nuno Oliveira <joaonunoao@gmail.com>
2021-01-04 19:02:18 +0000

    2015 day 02: sorting each list of dimensions makes this pretty easy

--- src/2015/day02.hs
+++ src/2015/day02.hs
@@ -8,13 +8,19 @@ format = decimal `sepBy` "x"
 
 paper :: [Int] -> Int
 paper dim = slack + sum [2 * x * y | (x:ys) <- tails dim, y <- ys]
-    where slack = product . take 2 $ sort dim
+    where slack = product . take 2 $ dim
+
+ribbon :: [Int] -> Int
+ribbon dim = bow + (sum . map (*2) . take 2 $ dim)
+    where bow = product dim
 
 -- |
 -- >>> :main
 -- 1606483
+-- 3842356
 main :: IO ()
 main = do
-    input <- readParsedLines 2015 2 format
+    input <- map sort <$> readParsedLines 2015 2 format
 
-    print . sum . map paper $ input
+    print . sum . map paper  $ input
+    print . sum . map ribbon $ input

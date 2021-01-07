a1047defce15535bf2243a920c11692bfb10c12d
João Nuno Oliveira <joaonunoao@gmail.com>
2021-01-07 20:24:13 +0000

    Add template Haskell file
    The idea is to iteratively refine this template and increase its
    usefulness.

---
+++ src/template.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE ImportQualifiedPost, OverloadedStrings, BlockArguments #-}
+
+import AdventAPI (readInputDefaults)
+import Advent.Megaparsec
+import Advent.Coord.Grid
+import Advent.Life
+import Advent.Math
+import Advent.Utils
+
+import Data.Bifunctor
+import Data.Char
+import Data.List
+import Data.Maybe
+
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Set (Set)
+import Data.Set qualified as Set
+
+-- |
+-- >>> :main
+-- 
+main :: IO ()
+main = do
+    -- input <- readInputDefaults YEAR DAY
+    -- input <- readParsedLines YEAR DAY format
+
+    print "TODO"

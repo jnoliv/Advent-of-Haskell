ccbe1aa1b310d46c82e404111c214e08a71c3a66
João Nuno Oliveira <joaonunoao@gmail.com>
2021-01-17 12:22:21 +0000

    2015 day 16: pattern matching is cool

---
+++ src/2015/day16.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Advent.Megaparsec
+import Data.List (find)
+
+type Prop = (String, Int)
+
+format :: Parser (Int, [Prop])
+format = (,) <$> ("Sue " *> decimal <* ": ")
+             <*> (property `sepBy` ", ")
+    where
+        property = (,) <$> many letterChar <* ": " <*> decimal
+
+filterAunts :: ([Prop] -> Prop -> Bool) -> [Prop] -> [(Int, [Prop])] -> Int
+filterAunts f props = fst . head . filter (all (f props) . snd)
+
+part1 :: [Prop] -> Prop -> Bool
+part1 props (k, v) = case find ((== k) . fst) props of
+    Just (_, n) -> n == v
+    Nothing     -> True
+
+part2 :: [Prop] -> Prop -> Bool
+part2 props (k, v) = case find ((== k) . fst) props of
+    Just ("cats", n)        -> v > n
+    Just ("trees", n)       -> v > n
+    Just ("pomeranians", n) -> v < n
+    Just ("goldfish", n)    -> v < n
+    Just (_, n)             -> n == v
+    Nothing                 -> True
+
+-- |
+-- >>> :main
+-- 40
+main :: IO ()
+main = do
+    aunts <- readParsedLines 2015 16 format
+
+    let tickertape = [("children", 3), ("cats", 7), ("samoyeds", 2), ("pomeranians", 3), ("akitas", 0),
+                      ("vizslas", 0), ("goldfish", 5), ("trees", 3), ("cars", 2), ("perfumes", 1)]
+
+    print $ filterAunts part1 tickertape aunts
+    print $ filterAunts part2 tickertape aunts
